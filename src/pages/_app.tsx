1038c99508fef07d440902258f842826d888f04b
Daniel Soares <daniel.soares@me.com>
2022-03-30 16:15:53 -0300

    add ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 
 import { AppProps } from 'next/app'
+import Script from 'next/script'
 import { Toaster } from 'react-hot-toast'
 import SuperTokensReact from 'supertokens-auth-react'
 
@@ -14,6 +15,20 @@ if (typeof window !== 'undefined') {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-673SLN2Q22"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-673SLN2Q22');
+        `}
+      </Script>
+
       <Toaster toastOptions={{ duration: 4000 }} />
       <Layout>
         <Component {...pageProps} />
